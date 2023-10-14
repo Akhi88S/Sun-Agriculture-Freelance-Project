90d6355d25b66871204b702caa69adfbca558396
Akhil Manikanta kanapala <akhilkanapala@Akhils-MacBook.local>
2023-10-14 21:50:47 +0530

    db_update

--- config/database.js
+++ config/database.js
@@ -1,3 +1,7 @@
 module.exports = {
-  database: " mongodb+srv://Akhil:1@cluster0-bskei.mongodb.net/test?retryWrites=true&w=majority"
-}
+  database:
+    "mongodb+srv://sun-admin:qAPBrn4wtdz7UIXC@cluster0.9bgmnzp.mongodb.net/",
+};
+//previous
+//   database:
+//     " mongodb+srv://Akhil:1@cluster0-bskei.mongodb.net/test?retryWrites=true&w=majority",
